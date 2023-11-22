3ba9f02f854af2d320bf5bdd67922d8b23f1ea7e
Nick2022 <876927113@qq.com>
2023-11-22 16:42:13 +0800

    try flow.

--- android/app/src/main/java/com/hele/android_native_architecture/MainActivity.kt
+++ android/app/src/main/java/com/hele/android_native_architecture/MainActivity.kt
@@ -10,24 +10,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.billy.cc.core.component.CC
 import com.elvishew.xlog.XLog
 import com.hele.android_native_architecture.ui.theme.Android_native_architectureTheme
 import com.hele.android_native_architecture.viewmodel.MainViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.zip
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
+
 
 class MainActivity : ComponentActivity() {
     private val mainViewModule: MainViewModel by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainViewModule.testKoin()
         setContent {
             Android_native_architectureTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
                 }
@@ -35,23 +41,87 @@ class MainActivity : ComponentActivity() {
         }
 
         testCC()
+
+        mainViewModule.apply {
+            testKoin()
+
+            lifecycleScope.launch(Dispatchers.IO) {
+                XLog.v("start flow 1.")
+//                testFlow1().collect {
+//                    XLog.d("test flow 1, collect value = $it")
+//                }
+//                XLog.v("start flow 2.")
+//                testFlow2().collect {
+//                    XLog.d("test flow 2, collect value = $it")
+//                }
+//
+//                testFlow4().collect {
+//
+//                }
+//                flow {
+//                    emit(testFlow3())
+//                }.catch {
+//
+//                }.collect {
+//
+//                }
+//
+//                ::testFlow3.asFlow().collect {
+//                    XLog.d("as flow test flow3 : $it")
+//                }
+
+                // transform start
+//                testFlow1().filter {
+//                    it < 3
+//                }.map {
+//                    "after map $it"
+//                }.flatMapConcat {
+//                    flow {
+//                        if (it.contains("1")) {
+//                            delay(300)
+//                        }
+//                        emit("flat map $it")
+//                    }
+//                }
+//                    .flowOn(Dispatchers.Default)
+//                    .collect {
+//                        XLog.d(it)
+//                    }
+
+//                merge(testFlow1(), testFlow2()).collect {
+//                    XLog.d("merge $it")
+//                }
+
+                testFlow1().zip(testFlow2()) { i: Int, j: Int ->
+                    "$i, $j"
+                }.catch {
+                    XLog.d("zip catch: $it")
+                }.onStart {
+                    XLog.d("flow start")
+                }
+                    .onCompletion {
+                        XLog.d("flow complete")
+                    }
+                    .collect {
+                        XLog.d("zip $it")
+                    }
+                // transform end
+
+            }
+        }
     }
 
     private fun testCC() {
         // 同步调用
-        val result = CC.obtainBuilder("ComponentTest")
-            .setActionName("toast")
-            .build().call()
+        val result = CC.obtainBuilder("ComponentTest").setActionName("toast").build().call()
 
         // 异步调用，不需要回调
-        val callId = CC.obtainBuilder("ComponentTest")
-            .setActionName("toast")
+        val callId = CC.obtainBuilder("ComponentTest").setActionName("toast")
 //            .addParam()   // 入参
             .build().callAsync()
 
         // 异步调用，需要回调
-        val callId2 = CC.obtainBuilder("ComponentTest")
-            .setActionName("toast")
+        val callId2 = CC.obtainBuilder("ComponentTest").setActionName("toast")
 //            .addParam()   // 入参
             .build().callAsync { cc, result ->
                 // 回调在子线程中
@@ -59,8 +129,7 @@ class MainActivity : ComponentActivity() {
             }
 
         // 异步调用，主线程回调
-        val callId3 = CC.obtainBuilder("ComponentTest")
-            .setActionName("toast")
+        val callId3 = CC.obtainBuilder("ComponentTest").setActionName("toast")
 //            .addParams()
             .build().callAsyncCallbackOnMainThread { cc, result ->
                 XLog.d("", "$result")
@@ -71,8 +140,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!", modifier = modifier
     )
 }
 
--- android/app/src/main/java/com/hele/android_native_architecture/viewmodel/MainViewModel.kt
+++ android/app/src/main/java/com/hele/android_native_architecture/viewmodel/MainViewModel.kt
@@ -2,11 +2,41 @@ package com.hele.android_native_architecture.viewmodel
 
 import com.elvishew.xlog.XLog
 import com.hele.base.viewmodel.BaseViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 
 class MainViewModel : BaseViewModel() {
 
     fun testKoin() {
         XLog.d("testKoin this.hashCode = ${this.hashCode()}")
     }
+
+    fun testFlow1(): Flow<Int> = flowOf(
+        1, 2, 3, 4, 5
+    )
+
+
+    fun testFlow2(): Flow<Int> = flow {
+        XLog.d("testFlow2 start")
+        emit(11)
+        delay(300)
+        emit(12)
+        delay(300)
+        emit(13)
+        delay(300)
+        XLog.d("after delay testFlow2")
+        emit(14)
+        XLog.d("testFlow2 end")
+    }
+
+    suspend fun testFlow3(): Int {
+        delay(200)
+        return 3
+    }
+
+    fun testFlow4() = ::testFlow3.asFlow()
 
 }
