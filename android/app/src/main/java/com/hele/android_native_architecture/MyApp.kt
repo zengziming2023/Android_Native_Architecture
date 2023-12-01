9ad474207f56c0d8116eb6914f39742a21a8412c
Nick2022 <876927113@qq.com>
2023-12-01 14:56:05 +0800

    add loadsir lib.

--- android/app/src/main/java/com/hele/android_native_architecture/MyApp.kt
+++ android/app/src/main/java/com/hele/android_native_architecture/MyApp.kt
@@ -3,6 +3,7 @@ package com.hele.android_native_architecture
 import com.hele.android_native_architecture.launcher.CCTask
 import com.hele.android_native_architecture.launcher.EpicTask
 import com.hele.android_native_architecture.launcher.KoinTask
+import com.hele.android_native_architecture.launcher.LoadSirTask
 import com.hele.base.BaseApplication
 import com.hele.base.launcher.task.Task
 
@@ -16,7 +17,8 @@ class MyApp : BaseApplication() {
         return listOf(
             CCTask(),
             EpicTask(),
-            KoinTask()
+            KoinTask(),
+            LoadSirTask()
         )
     }
 
---
+++ android/app/src/main/java/com/hele/android_native_architecture/launcher/LoadSirTask.kt
@@ -0,0 +1,13 @@
+package com.hele.android_native_architecture.launcher
+
+import com.hele.base.launcher.task.Task
+import com.kingja.loadsir.callback.ProgressCallback
+import com.kingja.loadsir.core.LoadSir
+
+class LoadSirTask : Task() {
+    override fun isRunOnMainThread(): Boolean = false
+
+    override fun run() {
+        LoadSir.beginBuilder().addCallback(ProgressCallback.Builder().build())
+    }
+}
