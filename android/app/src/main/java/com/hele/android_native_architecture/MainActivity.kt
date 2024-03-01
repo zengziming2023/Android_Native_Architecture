a40f7e9efb6335ca9e21cd4d14869778a42370c7
Nick2022 <876927113@qq.com>
2024-03-01 14:15:36 +0800

    practise compose Text and Image

--- android/app/src/main/java/com/hele/android_native_architecture/MainActivity.kt
+++ android/app/src/main/java/com/hele/android_native_architecture/MainActivity.kt
@@ -3,13 +3,35 @@ package com.hele.android_native_architecture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
 import com.billy.cc.core.component.CC
 import com.elvishew.xlog.XLog
@@ -167,15 +189,46 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!", modifier = modifier
-    )
+    Column(modifier = Modifier.padding(8.dp)) {
+        var text by remember {
+            mutableStateOf("2222")
+        }
+
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier
+                .background(Color.Blue, shape = RoundedCornerShape(8.dp))
+                .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 8.dp),
+            color = Color.Red,
+            fontSize = 18.sp,
+            fontWeight = Bold
+        )
+        Text(text = "Hello $text",
+            modifier = Modifier.clickable {
+                text = if (text == "2222") {
+                    "3333"
+                } else {
+                    "2222"
+                }
+            })
+        Spacer(modifier = Modifier.height(8.dp))
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "",
+            modifier = Modifier
+                .clip(CircleShape)
+                .background(colorResource(id = R.color.purple_200))
+                .size(width = 80.dp, height = 80.dp)
+                .border(2.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Android_native_architectureTheme {
-        Greeting("Android")
+        Greeting("Android Compose")
     }
 }
