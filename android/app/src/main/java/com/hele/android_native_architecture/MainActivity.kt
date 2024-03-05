d21d89d0f067d6fa93a7a673dea10fdca7958a37
Nick2022 <876927113@qq.com>
2024-03-05 13:54:55 +0800

    practise compose ContrainLayout.

--- android/app/src/main/java/com/hele/android_native_architecture/MainActivity.kt
+++ android/app/src/main/java/com/hele/android_native_architecture/MainActivity.kt
@@ -16,9 +16,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -34,8 +36,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalFocusManager
@@ -49,6 +49,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.lifecycleScope
 import coil.compose.rememberAsyncImagePainter
 import com.billy.cc.core.component.CC
@@ -214,6 +215,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun Greeting2(mainUIState: MainUIStateData) {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue, RoundedCornerShape(8.dp))
+    ) {
+        val (text1, button1, image1) = createRefs()
+        Button(onClick = {}, modifier = Modifier
+            .constrainAs(button1) {
+                top.linkTo(parent.top, margin = 8.dp)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+            .defaultMinSize(200.dp)) {
+            Text(text = mainUIState.name)
+        }
+
+        Text(text = mainUIState.text, modifier = Modifier
+            .constrainAs(text1) {
+                top.linkTo(button1.bottom, 8.dp)
+                start.linkTo(button1.start)
+            }
+            .background(Color.Red)
+            .padding(8.dp)
+            .clickable {
+
+            }
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "",
+            modifier = Modifier
+                .constrainAs(image1) {
+                    top.linkTo(text1.bottom, 8.dp)
+                    start.linkTo(button1.end)
+                }
+                .width(60.dp)
+        )
+
+    }
+}
+
+@Composable
 fun Greeting(mainUIState: MainUIStateData) {
     Column(modifier = Modifier.padding(8.dp)) {
         val focusManager = LocalFocusManager.current
@@ -334,6 +379,14 @@ private fun EditText() {
 fun GreetingPreview() {
     Android_native_architectureTheme {
         Greeting(MainUIStateData("Android Compose"))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Greeting2Preview() {
+    Android_native_architectureTheme {
+        Greeting2(MainUIStateData("Android Compose"))
     }
 }
 
